dae0044b01845093a9f500707ae36edd3c9a1b9a
hasan <hasanelmi678@gmail.com>
2023-01-18 11:43:25 +0000

    configured sanity client

---
+++ app/lib/sanity.js
@@ -0,0 +1,9 @@
+import sanityClient from '@sanity/client';
+
+export const client = sanityClient({
+    projectId: "8webwv81",
+    dataset: "production",
+    apiVersion: "2021-03-25",
+    token: process.env.SANITY_API_KEY,
+    useCdn: false,
+});
